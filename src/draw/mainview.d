3cc61c66313de0cf7789aaa39afa42793d4098ec
Anton Akzhigitov <akzhigitov.anton@yandex.ru>
2013-09-23 23:53:39 +0400

    start

---
+++ src/draw/mainview.d
@@ -0,0 +1,17 @@
+module draw.mainview;
+import desgui.base.widget;
+import desutil.signal;
+
+class MainView : Widget
+{
+    EmptySignal haltSignal;
+    this( string data_path )
+    {
+        super( null );
+
+        info.path = data_path;
+        info.font = "font/gost_a.ttf";
+        
+        keyboard.connectAlways( (p, k){ if( k.key == 27 ) haltSignal(); });
+    }
+}
--- src/main.d
+++ src/main.d
@@ -1,6 +1,18 @@
 import std.stdio;
+import desgui.glsdlapp;
+import desgui.base.widget;
+import std.path, std.file;
+import draw.mainview;
+import derelict.sdl2.sdl;
 
-void main()
+void main( string[] args )
 {
-    writeln( "Hello world" );
+    DerelictSDL2.load();
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+        throw new Exception( "Hui" );
+    GLSDLApp app = GLSDLApp.getApp();
+    auto mv = new MainView( buildNormalizedPath( dirName( args[0] ), "../data" ) );
+    mv.haltSignal.connect( &(app.haltEvent) );
+    app.setView( mv );
+    while( app.eventProcess() ){}
 }
