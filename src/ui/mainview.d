b55fcac5121a6119edae7cba2623f9dd888c7ab8
Anton Akzhigitov <akzhigitov.anton@yandex.ru>
2013-11-12 19:28:41 +0400

    done some functionality

--- src/ui/mainview.d
+++ src/ui/mainview.d
@@ -8,54 +8,146 @@ import ui.workspace;
 
 import desgl.draw.rectshape;
 
-class VLayout : Layout
+import std.stdio;
+
+enum
 {
-    enum
+    H_LAYOUT = true,
+    V_LAYOUT = false
+}
+
+enum 
+{
+    ALIGN_LEFT = 0,
+    ALIGN_TOP = 0,
+    ALIGN_RIGHT = 1,
+    ALIGN_BOTTOM = 1,
+    ALIGN_CENTER = 2,
+    ALIGN_JUSTIFY = 3
+}
+
+class LineLayout(bool L) : Layout
+{
+    private 
     {
-        ALIGN_RIGHT = 0,
-        ALIGN_CENTER,
-        ALIGN_LEFT
+        bool isStretched;
+        bool isJustify;
+        int mainoffset;
+        int seoffset;
+        int tboffset;
+        int al; 
     }
 
-    int offsety;
-    bool isStreched;
-    int lalign;
-
-    this( bool stry = false, int al = ALIGN_RIGHT, int oy = 2 )
+    this( bool stretched = true )
     {
-        isStreched = stry;
-        lalign = al;
-        offsety = oy;
+        isStretched = stretched;
     }
 
+    void setAlign( int val ){ al = val; }
+    void setJustify( bool val ){ isJustify = val; }
+    void setSEOffset( int val ){ mainoffset = val; }
+    
     void opCall( irect r, Widget[] ws )
     {
-        int summ;
-        if( isStreched )
+        int f_size, s_size;
+
+        int offset;
+        irect trect;
+
+        int mval;
+
+        if( L == H_LAYOUT )
+            mval = r.w;
+        else
+            mval = r.h;
+
+        int f_count, s_count;
+
+        bool isFix( Widget w )
         {
-            foreach( w; ws )
-                summ += w.rect.h;
-            offsety = cast(int)( ( r.h - summ ) / ( ws.length ));
+            if( L == H_LAYOUT )
+                return w.lims.w.fix;
+            return w.lims.h.fix;
         }
-        summ = offsety / 2;
-        irect t;
-        int ox;
-        if( lalign == ALIGN_LEFT )
-            ox = 5;
-        foreach( ref w; ws )
+        foreach( w; ws )
         {
-            if( lalign == ALIGN_RIGHT )
-                ox = r.w - w.rect.w - 5;
-            if( lalign == ALIGN_CENTER )
-                ox = r.w / 2 - w.rect.w / 2;
-            t = irect( ox, summ, w.rect.w, w.rect.h );
-            w.reshape( t );
-            summ += offsety + w.rect.h;
+            if( L == H_LAYOUT )
+                if( w.lims.w.fix )
+                {
+                    f_size += w.rect.w;
+                    f_count++;
+                }
+            if( L == V_LAYOUT )
+                if( w.lims.h.fix )
+                {
+                    f_size += w.rect.h;
+                    f_count++;
+                }
         }
+        s_count = cast(int)(ws.length) - f_count;
+        if( s_count != 0 )
+            s_size = ( mval - f_size ) / s_count;
+
+        if( isStretched )
+        {
+            int poff;
+            int n_size;
+            foreach( ref w; ws )
+            {
+                if( isFix(w) )
+                {
+                    if( L == H_LAYOUT )
+                    {
+                        trect = irect( offset, 0, w.rect.w, r.h );
+                        offset += w.rect.w;
+                    }
+                    else
+                    {
+                        trect = irect( 0, offset, r.w, w.rect.h );
+                        offset += w.rect.h;    
+                    }
+                }
+                else
+                {
+                    if( L == H_LAYOUT )
+                    {
+                        n_size = w.lims.w( w.rect.w, s_size );
+                        trect = irect( offset, 0, n_size, r.h );
+                    }
+                    else
+                    {
+                        n_size = w.lims.h( w.rect.h, s_size );
+                        trect = irect( 0, offset, r.w, n_size );
+                    }
+                    s_count--;
+                    if( n_size != s_size )
+                    {
+                        s_size = ( mval - f_size - n_size ) / s_count;
+                    }
+                    offset += n_size;
+                }
+                w.reshape( trect );
+            }
+        }
+        //else
+        //{
+        //    int toffset;
+        //    if( L == H_LAYOUT )
+        //       toffset 
+        //    if( isJustify )
+        //    {
+        //        if( L == H_LAYOUT )
+
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //}
     }
 }
 
-class ToolBar( int A ) : Widget
+class ToolBar : Widget
 {
 public:
     ColorRect shape;
@@ -78,31 +170,40 @@ public:
                     shape.reshape(inrect);
                 });
         reshape( r );
-        layout = new VLayout( false, A, 10 );
-        bool grabed;
-        ivec2 old_vec;
-        ivec2 grab_vec;
+        size_lim.w.fix = true;
+        layout = new LineLayout!V_LAYOUT(false);
+        //ivec2 old_vec;
+        //ivec2 grab_vec;
 
-        mouse.connect( ( in ivec2 p, in MouseEvent me )
-                {
-                    if( me.type == me.Type.PRESSED && 
-                        p.x > rect.w-20 )
-                    {
-                        writeln( "Grabed" );
-                        grabed = true;
-                        grab_vec = p - rect.size;
-                    }
-                    if( me.type == me.Type.RELEASED )
-                    {
-                        grabed = false;
-                        writeln( "Ungrabed" );
-                    }
-                    if( grabed )
-                    {
-                        auto inrect = irect( rect.pos , p.x - grab_vec.x, rect.h  );
-                        reshape( inrect );
-                    }
-                });
+        //mouse.connect( ( in ivec2 p, in MouseEvent me )
+        //        {
+        //            if( me.type == me.Type.PRESSED && 
+        //                p.x > rect.w-20 )
+        //            {
+        //                grab = true;
+        //                grab_vec = p - rect.size;
+        //            }
+        //            if( me.type == me.Type.RELEASED )
+        //            {
+        //                grab = false;
+        //            }
+        //            if( grab )
+        //            {
+        //                auto inrect = irect( rect.pos , p.x - grab_vec.x, rect.h  );
+        //                reshape( inrect );
+        //                parent.reshape( parent.rect );
+        //            }
+        //        });
+    }
+}
+
+class MyButton : SimpleButton
+{
+    this( Widget par, irect r, wstring caption, void delegate() onclick = null )
+    {
+        super( par, r, caption, onclick );
+        size_lim.w.fix = false;
+        size_lim.h.fix = true;
     }
 }
 
@@ -110,24 +211,15 @@ class MainView : AppWindow
 {
 private:
 
-    class MLayout : Layout
-    {
-        void opCall( irect r, Widget[] ws )
-        {
-            tb.reshape( irect( 0, 0, tb.rect.w, r.h ) ); 
-            wspace.reshape( irect( tb.rect.w, 0, r.w - tb.rect.w, r.h ) );
-        }
-    }
-
 public:
-    ToolBar!(VLayout.ALIGN_LEFT) tb;
+    ToolBar tb;
     WorkSpace wspace;
     this( string title )
     {
         super( title );
 
-        tb = new ToolBar!(VLayout.ALIGN_LEFT)( this, irect( 0, 20, 100, 400 ) );
-        wspace = new WorkSpace( this, irect( 130, 5, 400, 400 ) );
+        tb = new ToolBar( this, irect( 0, 20, 100, 400 ) );
+        wspace = new WorkSpace( this, irect( 130, 5, 40, 40 ) );
 
         SimpleButton[10] btn1;
         import std.random;
@@ -135,11 +227,10 @@ public:
         {
             import std.string;
             import std.conv;
-            b = new SimpleButton( tb, irect( i, 5, 30+uniform( 0, 20 ), 30 ), to!wstring(format( "%d", i )), {} );
+            b = new MyButton( tb, irect( i, 5, 30+uniform( 0, 20 ), 30 ), to!wstring(format( "%d", i )), {} );
         }
 
-        layout = new MLayout();
+        layout = new LineLayout!H_LAYOUT;
 
-        import std.stdio;
     }
 }
