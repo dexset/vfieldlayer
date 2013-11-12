0f8610c803ed736802aa6251987604408a0b66d9
deviator <code.viator@gmail.com>
2013-11-12 22:57:48 +0400

    minimal changes, remove some tests

--- src/engine/core/layer.d
+++ src/engine/core/layer.d
@@ -2,6 +2,7 @@ module engine.core.layer;
 
 public import engine.core.setting;
 public import engine.core.image;
+public import desmath.types.vector;
 public import desmath.types.rect;
 
 interface Layer: SettingObject
@@ -14,4 +15,7 @@ interface Layer: SettingObject
         bool select() const;
         void select( bool );
     }
+
+    void move( in ivec2 );
+    void setPos( in ivec2 );
 }
--- src/engine/package.d
+++ src/engine/package.d
@@ -1,2 +1,5 @@
 module engine;
 
+public import engine.program;
+public import engine.setting;
+public import engine.workspace;
