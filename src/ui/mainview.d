bfbd560874d7940fb1b0eec302a15e0f7f763e32
deviator <code.viator@gmail.com>
2013-11-12 04:18:26 +0400

    example of bad draw text

--- src/ui/mainview.d
+++ src/ui/mainview.d
@@ -12,7 +12,18 @@ class MainView : AppWindow
     {
         super( title );
 
-        auto btn1 = new SimpleButton( this, irect( 5, 5, 200, 30 ), "button1"w, {} );
+        size_t btn1_cond = 0;
+        SimpleButton btn1;
+        wstring[] btn_str = [ 
+            "as",
+            "df",
+            "jh",
+            "jg",
+            "10",
+            "j1"
+            ];
+        btn1 = new SimpleButton( this, irect( 5, 5, 200, 30 ), "button1"w, 
+                { btn1.label.setText( btn_str[btn1_cond%$] ); btn1_cond++; } );
         auto btn2 = new SimpleButton( this, irect( 5, 40, 200, 30 ), "button2"w, {} );
 
         auto ws = new WorkSpace( this, irect( 205, 5, 400, 400 ) );
