4035d5944ed0234e1d96235d357e3677a64c909b
deviator <code.viator@gmail.com>
2013-11-12 21:41:49 +0400

    add core package file

---
+++ src/engine/core/package.d
@@ -0,0 +1,16 @@
+module engine.core;
+
+public import engine.core.action;
+public import engine.core.drawtool;
+public import engine.core.filter;
+public import engine.core.history;
+public import engine.core.image;
+public import engine.core.item;
+public import engine.core.layer;
+public import engine.core.programiface;
+public import engine.core.setting;
+public import engine.core.tempbuffer;
+public import engine.core.tool;
+public import engine.core.viewport;
+public import engine.core.viewtool;
+public import engine.core.wsdata;
