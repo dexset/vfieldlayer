8b518ae2f428c95a8d189a1c1af3bd7efd3e9cb6
deviator <code.viator@gmail.com>
2013-12-01 21:28:11 +0400

    some change in mouse processing

--- src/ui/workspace.d
+++ src/ui/workspace.d
@@ -13,36 +13,34 @@ class WorkSpace: DiWidget
 {
 private:
 
-    lim_t!float zoom_lim = lim_t!float(0.1, 10000);
+    lim_t!float zoom_lim = lim_t!float(0.001, 10000);
     float zoom = 1.0f;
 
     ivec2 old_mpos;
 
     void mouse_hook( in ivec2 p, in DiMouseEvent me )
     {
-        if( me.type == me.Type.RELEASED && 
-            me.btn == me.Button.MIDDLE )
+        if( me.type == me.Type.RELEASED && me.btn == me.Button.MIDDLE )
             grab = 0;
         else 
-        if( me.type == me.Type.PRESSED && 
-            me.btn == me.Button.MIDDLE )
+        if( me.type == me.Type.PRESSED && me.btn == me.Button.MIDDLE )
             grab = 1;
         else 
-        if( me.type == me.Type.MOTION && grab )
-            inner += p - old_mpos;
+        if( me.type == me.Type.MOTION && grab ) inner += p - old_mpos;
 
         if( me.type == me.Type.WHEEL )
         {
             zoom = zoom_lim( zoom, zoom + me.data.y * zoom * 0.2 );
-
             auto lp = p - rect.pos;
             inner += ivec2( -(lp-inner) * me.data.y * zoom );
+            im.reshape( irect( 0, 0, ivec2(640,480) * zoom ) );
         }
 
         old_mpos = p;
     }
 
     GLTexture2D tex;
+    DiImage im;
 
 public:
     this( DiWidget par, in irect r )
@@ -51,7 +49,7 @@ public:
         reshape( r );
 
         Image image = loadImageFromFile( "data/images/im1.jpg" );
-        auto im = new DiImage( this, irect( 20, 20, 640, 480 ), image );
+        im = new DiImage( this, irect( 20, 20, 640, 480 ), image );
         tex = new GLTexture2D;
         auto baserect = irect( 5,5, image.size );
         tex.image( image );
@@ -62,17 +60,6 @@ public:
 
         auto plane = new ColorTexRect!()( ploc, cloc, tloc );
         plane.reshape( irect(10, 10, image.size) );
-
-        reshape.connect( (r)
-        {
-            im.reshape(irect(0, 0, r.size)); 
-        });
-        draw.connect( ()
-        {
-            //im.draw();
-        });
-
-        idle.connect( { im.reshape( irect( 0, 0, im.rect.size * zoom ) ); } );
 
         mouse.connectAlways( &mouse_hook );
     }
