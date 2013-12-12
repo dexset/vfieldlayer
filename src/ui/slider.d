701a0e9e17068c61f42ded26a00ba58960e5161a
Anton Akzhigitov <akzhigitov.anton@yandex.ru>
2013-12-13 00:43:01 +0400

    some changes

--- src/ui/slider.d
+++ src/ui/slider.d
@@ -96,6 +96,7 @@ public:
         panel = new ColorRect!()( ploc, cloc );
 
         slider = new SimpleButton( this, irect( 0, 0, 10, sz.y ) );
+        slider.activate();
         auto drag = false;
 
         slider.mouse.connect( (p, me)
